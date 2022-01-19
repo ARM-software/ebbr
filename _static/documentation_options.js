2ae4d1e8e6ecbcffd81a8f175c69d354564623ab
glikely <glikely@users.noreply.github.com>
2022-01-19 17:38:40 +0000

    deploy: c90b128aad070d8c015b6b434e87e9431e2c93a0

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,10 +1,11 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2.0.0',
+    VERSION: 'v2.0.1-8-gc90b128',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'singlehtml',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false

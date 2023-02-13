8b1f418062fbdf2e258d68c89fd0fa5e0ecc70cf
vstehle <vstehle@users.noreply.github.com>
2023-02-13 18:46:13 +0000

    deploy: 38a005b127b67ca4fb06550d485806e38ec83c08

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2.1.0-7-ge68c3ce',
+    VERSION: 'v2.1.0-8-g38a005b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'singlehtml',

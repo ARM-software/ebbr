61dfa28e5ff352f55fd2993abb0f31640f96f42c
vstehle <vstehle@users.noreply.github.com>
2026-02-11 17:21:47 +0000

    deploy: 2d6dd86e6d7f0d979575a272d6ecd7c379cd6b9d

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: 'v2.4.0-pre2-6-gce40af1',
+    VERSION: 'v2.4.0-pre2-7-g2d6dd86',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'singlehtml',
